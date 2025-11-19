3041a7a3ebde3f3e64b72354e089dea8b1a6dd7e
Misael <0932882@my.scccd.edu>
2025-11-19 23:30:19 +0000

    finished code for 14.6 and 14.7

--- Nunez14_6.m
+++ Nunez14_6.m
@@ -1,1 +1,35 @@
+Tb = 170;
+T0 = 70;
+tau = 10;
+
+fprintf('Temperature Application\n');
+fprintf('Initial Temp = %.1f F, Bath Temp = %.1f F\n\n', T0, Tb);
+
+% Temperature function:
+times = [10 20 40];
+
+for i= 1:length(times)
+    t  =times(i);
+    Tt = Tb + (T0 - Tb)*exp(-t/tau);
+ fprintf('Temperature at t = %2d s is %.2f F\n', t, Tt);
+end
+%TIME TO REACH
+T_target = 160;
+
+% Solve eequation
+t160  = -tau * log((T_target - Tb)/(T0 - Tb));
+
+fprintf('\nTime to reach %.1f F is %.1f seconds\n\n', T_target, t160);
+
+%temp first 40 sec
+t_plot =0:0.1:40;
+T_plot= Tb + (T0 - Tb)*exp(-t_plot/tau);
+
+figure;
+plot(t_plot,  T_plot, 'LineWidth', 1.4);
+grid on;
+xlabel('Tnime (s)');
+ylabel('Temperature (F)');
+title('Temperature vs Time for First 40 Seconds');
+  ylim([0 180]);
 
--- Nunez14_7.m
+++ Nunez14_7.m
@@ -1,1 +1,27 @@
+m = 1000;
+c = 500; 
+f_list =  [20000 40000 60000 80000];
+
+ fprintf('Rocket Propulsion Application\n');
+fprintf('Mass = %d kg, Resistance Constant = %d N*s/m\n\n', m, c);
+
+% Velocity formula:
+t  = 0:0.1:10;
+
+figure ;
+hold  on;
+
+for f = f_list
+    v =(f/c) .* (1 - exp(-(c/m).*t));
+ plot(t,  v, 'LineWidth', 1.4);
+
+    fprintf('Plotted velocity for thrust f = %d N\n', f);
+end
+
+grid on;
+xlabel('Time (s)');
+ylabel('Velocit,y (m/s)');
+title('Rocket Velocity for Different Thrust Values');
+legend('20,000 N','40,000 N','60,000 N','80,000 N', 'Location','best');
+hold off;
 
